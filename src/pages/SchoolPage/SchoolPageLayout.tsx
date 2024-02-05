7f3e0899ad5220ba93d21eff6313517a4defe07b
lumi808 <108948664+lumi808@users.noreply.github.com>
2024-02-05 11:37:23 +0400

    feat: school pages content

--- src/pages/SchoolPage/SchoolPageLayout.tsx
+++ src/pages/SchoolPage/SchoolPageLayout.tsx
@@ -1,9 +1,79 @@
 import React from 'react';
+import {Box, Grid, CardContent, CardMedia, Typography} from '@mui/material';
+import { SchoolPageHeader } from "@src/pages/SchoolPage/components/SchoolPageHeader";
+import styles from "./SchoolPage.module.css";
+import { useNavigate } from 'react-router-dom';
+import exampleImage from "@src/assets/example/schoolExample.jpeg";
 
 export const SchoolPageLayout: React.FC = () => {
+    const navigate = useNavigate();
+    const schoolsList: any = [
+        {
+            id: 1,
+            name: "NIS IB",
+
+        },
+        {
+            id: 2,
+            name: "Miras",
+
+        }
+    ];
+    const baseURL = process.env.REACT_APP_ADMIN_API_BASE_URL;
+
     return (
-        <div>
-            <h1>School Page</h1>
-        </div>
+        <Box display='flex' flexWrap='wrap' justifyContent='center' gap='0 1rem' className={styles.mainContainer}
+            pt='2rem'>
+            <SchoolPageHeader/>
+            <Grid container display="flex" rowSpacing={1} columnSpacing={1} flexWrap="wrap" 
+                justifyContent="space-between" className={styles.schoolContainer} width='100%'
+            >
+                {schoolsList.map((school: any) => (
+                    <Grid
+                        key={school.id} item xs={12} sm={5.9} md={5.9}
+                        lg={5.9}
+                        onClick={() => {
+                            navigate(`/school/${school.id}`);
+                        }}
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            cursor: "pointer",
+                            borderRadius: "1.25rem",
+                            marginBottom: "1.5rem", backgroundColor: 'white',
+                            paddingTop: ".5rem !important",
+                            paddingX: ".5rem !important",
+
+                        }}
+                    >
+                        <CardMedia
+                            component="img"
+                            className={styles.schoolImg}
+                            sx={{
+                                width: "100%",
+                                borderRadius: "10px",
+                            }}
+                            image={school.banner ? `${baseURL}/${school.banner}` : exampleImage}
+                            alt={school.name ? `${school.name}` : "School image"}
+                        />
+                        <Box sx={{display: 'flex', flexDirection: 'column', width: "100%"}}>
+                            <CardContent sx={{flex: '0 0 auto'}}>
+                                <Typography mb='.5rem' fontSize="1.3rem" fontWeight="600" sx={{
+                                    "@media (max-width: 778px)": {
+                                        fontSize: '0.75rem'
+                                    },
+                                    "@media (max-width: 998px)": {
+                                        fontSize: '1rem'
+                                    },
+                                }}>
+                                    {school.name}
+                                </Typography>
+                            </CardContent>
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     );
 };
---
+++ src/pages/SchoolPage/generator.tsx
@@ -0,0 +1,26 @@
+export const localization = {
+	"kz":{
+		Header:{
+			filter: "Фильтр",
+			searchBar: "Мектеп атауы",
+			searchButton: "Іздеу",
+			university: "Мектептер",
+		},
+	},
+	"ru":{
+		Header:{
+			filter: "Фильтр",
+			searchBar: "Название школы",
+			searchButton: "Поиск",
+			university: "Школы",
+		},
+	},
+	"en":{
+		Header:{
+			filter: "Filter",
+			searchBar: "School name",
+			searchButton: "Search",
+			university: "Schools",
+		},
+	},
+}
