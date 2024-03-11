90380b4d1b9b78e9132ee34eda87962735bb2d30
lumi808 <108948664+lumi808@users.noreply.github.com>
2024-03-11 22:16:22 +0400

    fix: students search results

--- src/components/HiringPopup/StudentCard.tsx
+++ src/components/HiringPopup/StudentCard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { CardActionArea, IconButton } from '@mui/material';
 import {isAuthenticated} from "@src/utils/userAuth";
 import { useDispatch, useSelector } from 'react-redux';
@@ -20,7 +21,7 @@ export const StudentCard: React.FC<StudentCardProps> = (props) => {
     const role = useSelector(selectUserRole);
     const dispatch = useDispatch();
     const [isFavorite, setIsFavorite] = React.useState(false);
-    
+
     const handleCardClick = (counter: number) => {
         if (role === 'Student') {
             setAlertOpen(true);
@@ -52,34 +53,31 @@ export const StudentCard: React.FC<StudentCardProps> = (props) => {
     };
 
     return (
-        <Card 
-            sx={{ maxWidth: 345 }}
+        <Card
+            sx={{ display: 'flex', width: '19.5rem', padding: '0.75rem', borderRadius: '1.25rem', height: '100%' }}
         >
             <CardActionArea
                 onClick={ () => {handleCardClick(student.id)} }
             >
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {student.name_ru}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                <CardContent sx={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', width: '100%'}}>
+                    <Box sx={{display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '0.5rem'}}>
+                      <Typography gutterBottom component="div" sx={{fontSize: '1.25rem', fontWeight:600, textAlign: 'left'}}>
+                          {student.name_ru}
+                      </Typography>
+                      <IconButton
+                          sx={{ width: "2.5rem", height: "2.5rem",backgroundColor: 'transparent'}}
+                          onClick={handleToogleFavoriteDiplomas}
+                      >
+                          <FavoriteDiploma fill={isFavorite ? "#3B82F6" : "#D8E6FD"}/>
+                      </IconButton>
+                    </Box>
+                    <Typography color="text.secondary" sx={{fontSize: '1rem', fontWeight:400, textAlign: 'left'}}>
                         {student.speciality_ru}
                     </Typography>
-                    
+
                 </CardContent>
             </CardActionArea>
-            <IconButton
-                sx={{
-                    width: "2.5rem",
-                    height: "2.5rem",
-                    backgroundColor: "#D8E6FD",
-                    "@media (max-width: 778px)": {
-                        display: 'none'
-                    }
-                }}
-                onClick={handleToogleFavoriteDiplomas}>
-                <FavoriteDiploma fill={isFavorite ? "#3B82F6" : "white"}/>
-            </IconButton>
+
         </Card>
     );
 }
